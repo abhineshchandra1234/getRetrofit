b77b102aab95b8358e9ae69edf40179bbdad8f7e
abhinesh <abhinesh.c@eegrab.com>
2021-01-05 06:47:57 +0530

    second commit

--- app/src/main/java/com/abhinesh/getretrofit/MainActivity.kt
+++ app/src/main/java/com/abhinesh/getretrofit/MainActivity.kt
@@ -3,48 +3,42 @@ package com.abhinesh.getretrofit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.abhinesh.getretrofit.adapter.MyAdapter
 import com.abhinesh.getretrofit.repository.Repository
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private val myAdapter by lazy { MyAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupRecyclerview()
+
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
         viewModel = ViewModelProvider(this,viewModelFactory).get(MainViewModel::class.java)
-        //viewModel.getPost()
-
-        val options: HashMap<String,String> = HashMap()
-        options["_sort"] = "id"
-        options["_order"] = "desc"
-
-
-
-        button.setOnClickListener {
-            val myNumber = number_editText.text.toString()
-            viewModel.getCustomPosts2(Integer.parseInt(myNumber), options)//asc//desc
-
-            viewModel.myCustomPosts2.observe(this, Observer { response ->
+        viewModel.getCustomPosts(5,"id","desc")
+        viewModel.myCustomPosts.observe(this, Observer {response ->
                 if (response.isSuccessful){
-                    tvHello.text = response.body().toString()
-                    response.body()?.forEach {
-                        Log.d("Response",it.userId.toString())
-                        Log.d("Response",it.id.toString())
-                        Log.d("Response",it.title)
-                        Log.d("Response",it.body)
-                        Log.d("Response","-------------------------------")
-                    }
-                } else {
-                    tvHello.text = response.code().toString()
+                    response.body()?.let { myAdapter.setData(it) }
+                }else {
+                    Toast.makeText(this,response.code(), Toast.LENGTH_SHORT).show()
                 }
-            })
-        }
+        })
+
+    }
+
+    private fun setupRecyclerview(){
+        recyclerView.adapter = myAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
